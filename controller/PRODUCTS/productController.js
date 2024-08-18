115c58fbda9e104b28168dc54862e4f2e644a43a
the.sholaakanbi <oluwalonshola6@gmail.com>
2024-08-18 22:17:11 +0100

    correction of codes

--- controller/PRODUCTS/productController.js
+++ controller/PRODUCTS/productController.js
@@ -20,13 +20,13 @@ const getAllProducts = asyncWrapper(async (req, res) => {
 
 // Get a single product by custom ID
 const getProduct = asyncWrapper(async (req, res) => {
-    const { id: productid  } = req.params;
+    const { id: productId  } = req.params;
 
     try {
-        const product = await Product.findOne({ id: productid });
+        const product = await Product.findOne({ id: productId });
 
         if (!product) {
-            return res.status(404).json({ msg: `Product with the id: ${productid} not found` });
+            return res.status(404).json({ msg: `Product with the id: ${productId} not found` });
         }
         res.status(200).json(product);  // Return the actual product object
     } catch (err) {
@@ -74,7 +74,7 @@ const createProduct = asyncWrapper(async (req, res) => {
 
 // Update a product by custom ID
 const updateProduct = asyncWrapper(async (req, res) => {
-    const { productId } = req.params;
+    const { id: productId } = req.params;
 
     try {
         const updatedProduct = await Product.findOneAndUpdate(
